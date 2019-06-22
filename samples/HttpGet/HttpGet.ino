a2a78bd26aa68ab2e72c8dd2753b6977e190d5c7
tigerfarm <tigerfarm@gmail.com>
2019-06-21 18:18:34 -0700

    updates

--- samples/HttpGet/HttpGet.ino
+++ samples/HttpGet/HttpGet.ino
@@ -1,67 +1,61 @@
 #include <ESP8266WiFi.h>
 
-const char *ssid = "BATCAVE";  //ENTER YOUR WIFI SETTINGS
-const char *password = "5198427760";
+const char *ssid = "BATCAVE";  // WIFI SETTINGS: Network name (SSID) and password.
+// const char *password = "mypassword";
+const char *password = "";  // Note, I don't save my password when I upload to the repository.
 
 // const char* host = "tigerfarmpress.com";
 const char *host = "192.168.1.73";
 
+int loopCounter = 0;  // loop counter
 void setup() {
   Serial.begin(115200);
   delay(10);
-  // We start by connecting to a WiFi network
   Serial.println();
   Serial.println();
-  Serial.print("Connecting to ");
+  // ------------------------------------------------
+  Serial.print("+ Connecting to the WiFi network: ");
   Serial.println(ssid);
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
     Serial.print(".");
+    ++loopCounter;
   }
   Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
+  Serial.print("++ WiFi connected on IP address: ");
   Serial.println(WiFi.localIP());
+  loopCounter = 0;
 }
-
-int value = 0;
 
 void loop() {
   delay(10000);
-  ++value;
-
-  Serial.print("connecting to ");
+  ++loopCounter;
+  Serial.println("-------------------------------------------------------");
+  Serial.print("+ Connecting to: ");
   Serial.println(host);
-
-  // Use WiFiClient class to create TCP connections
-  WiFiClient client;
+  WiFiClient client;  // Use WiFiClient class to create TCP connections
   const int httpPort = 8000;
   if (!client.connect(host, httpPort)) {
     Serial.println("connection failed");
     return;
   }
-
-  // We now create a URI for the request
-  // String url = "/stan";
   String url = "/hello.txt";
-
-  Serial.print("Requesting URL: ");
+  Serial.print("++ Connected. Make request to the URL: ");
   Serial.println(url);
-
-  // This will send the request to the server
-  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
-               "Host: " + host + "\r\n" +
-               "Connection: close\r\n\r\n");
-  delay(10);
-
-  // Read all the lines of the reply from server and print them to Serial
-  Serial.println("Respond:");
+  client.print(
+    String("GET ") + url + " HTTP/1.1\r\n"
+    + "Host: " + host + "\r\n"
+    + "Connection: close\r\n\r\n"
+  );
+  Serial.println("-----------------");
+  delay(100);
+  Serial.println("Response:");
   while (client.available()) {
     String line = client.readStringUntil('\r');
     Serial.print(line);
   }
-
   Serial.println();
-  Serial.println("closing connection");
+  Serial.println("-----------------");
+  Serial.println("+ Closing connection");
 }
