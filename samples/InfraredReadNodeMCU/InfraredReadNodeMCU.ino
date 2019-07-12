64ed27c4d219333a5607facefca67b39a168efa6
tigerfarm <tigerfarm@gmail.com>
2019-07-12 10:31:42 -0700

    updates

--- samples/InfraredReadNodeMCU/InfraredReadNodeMCU.ino
+++ samples/InfraredReadNodeMCU/InfraredReadNodeMCU.ino
@@ -1,9 +1,3 @@
-/****************************
-  Infrared Wireless Remote Control Kit for Arduino DIY Project
-  https://www.instructables.com/id/Universal-Remote-Using-ESP8266Wifi-Controlled/
-
-*/
-
 // -----------------------------------------------------------------------------
 // Infrared settings
 
@@ -37,53 +31,63 @@ void setup() {
   Serial.println("+++ Setup.");
   //
   irrecv.enableIRIn();
+  Serial.println("+ Setup completed.");
 }
 
 // -----------------------------------------------------------------------
 void infraredSwitch() {
   // Serial.println("+ infraredSwitch");
   //
+  // Top case value is for small remote controller.
+  // Lower case value is for Samsung TV remote controller.
+  //
   switch (results.value) {
     case 0xFFFFFFFF:
-      // Ignore. This is from holding the key down.
+    case 0xFFFFFFFFFFFFFFFF:
+      // Ignore. This is from holding the key down on small remote controller.
+      // When holding a key down on the Samsung remote, get arbitrary result values.
+      Serial.print(".");
       break;
-
     // -----------------------------------
     case 0xFFA25D:
-    case 0xFFA25D:
-      Serial.print("+ Key 1: ");
+    case 0xE0E020DF:
+      Serial.println("+ Key 1: ");
       break;
     case 0xFF629D:
-    case 0xFF629D:
-      Serial.print("+ Key 2: ");
+    case 0xE0E0A05F:
+      Serial.println("+ Key 2: ");
       break;
     case 0xFFE21D:
-    case 0xFFE21D:
-      Serial.print("+ Key 3: ");
+    case 0xE0E0609F:
+      Serial.println("+ Key 3: ");
       break;
     case 0xFF22DD:
-    case 0xFF22DD:
-      Serial.print("+ Key 4: ");
+    case 0xE0E010EF:
+      Serial.println("+ Key 4: ");
       break;
     case 0xFF02FD:
-    case 0xFF02FD:
-      Serial.print("+ Key 5: ");
+    case 0xE0E0906F:
+      Serial.println("+ Key 5: ");
       break;
     case 0xFFC23D:
-    case 0xFFC23D:
-      Serial.print("+ Key 6: ");
+    case 0xE0E050AF:
+      Serial.println("+ Key 6: ");
       break;
     case 0xFFE01F:
-    case 0xFFE01F:
-      Serial.print("+ Key 7: ");
+    case 0xE0E030CF:
+      Serial.println("+ Key 7: ");
       break;
     case 0xFFA857:
-    case 0xFFA857:
-      Serial.print("+ Key 8: ");
+    case 0xE0E0B04F:
+      Serial.println("+ Key 8: ");
       break;
     case 0xFF906F:
-    case 0xFF906F:
-      Serial.print("+ Key 9: ");
+    case 0xE0E0708F:
+      Serial.println("+ Key 9: ");
+      break;
+    case 0xFF9867:
+    case 0xE0E08877:
+      Serial.println("+ Key 0: ");
       break;
     // -----------------------------------
     case 0xFF10EF:
@@ -94,26 +98,28 @@ void infraredSwitch() {
     case 0xE0E046B9:
       Serial.println("+ Key > - next");
       break;
+    case 0xFF38C7:
     case 0xE0E016E9:
       Serial.println("+ Key center, OK key");
       break;
     case 0xFF18E7:
     case 0xE0E006F9:
-      Serial.print("+ Key up - next directory");
+      Serial.println("+ Key up");
       break;
     case 0xFF4AB5:
     case 0xE0E08679:
-      Serial.print("+ Key down - previous directory");
+      Serial.println("+ Key down");
       break;
     // -----------------------------------
-    // Single song loop
     case 0xFF6897:
     case 0xE0E01AE5:
-      Serial.println("+ Key * (Return)");
+    case 0xE0E0C43B:
+      Serial.println("+ Key *, button left of '0' or Return.");
       break;
     case 0xFFB04F:
     case 0xE0E0B44B:
-      Serial.println("+ Key # (Exit)");
+    case 0xE0E0C837:
+      Serial.println("+ Key #, button right of '0' or Exit.");
       break;
     // -----------------------------------
     default:
@@ -122,20 +128,21 @@ void infraredSwitch() {
       Serial.println("");
       // -----------------------------------
   } // end switch
+  
+  delay(60);  // To reduce repeat key results.
 }
 
 // -----------------------------------------------------------------------------
 // Device Loop
 
 int loopCounter = 0;
-int iPosition = 0;
 
 void loop() {
-  delay(2000);
-  blinkLed();
+  // blinkLed();
+  delay(60);
   ++loopCounter;
-  Serial.print("+ loopCounter = ");
-  Serial.println(loopCounter);
+  // Serial.print("+ loopCounter = ");
+  // Serial.println(loopCounter);
   //
   if (irrecv.decode(&results)) {
     infraredSwitch();
