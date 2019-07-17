70acea1460a22c5f6ff8ab1274692a858a479468
tigerfarm <tigerfarm@gmail.com>
2019-07-17 14:09:42 -0700

    updates

--- samples/IrNodemcuHttpWsSync/IrNodemcuHttpWsSync.ino
+++ samples/IrNodemcuHttpWsSync/IrNodemcuHttpWsSync.ino
@@ -63,7 +63,7 @@ void blinkLed() {
 // WIFI SETTINGS: Network name (SSID) and password.
 
 const char *ssid = "BATCAVE";
-const char *password = "5198427760";  // Note, I don't save my password on the repository.
+const char *password = "";  // Note, I don't save my password on the repository.
 
 // -----------------------------------------------------------------------------
 
