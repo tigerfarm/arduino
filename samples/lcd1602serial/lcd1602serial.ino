f249954ea135664ac8c349e3087ba92a3d454b22
tigerfarm <tigerfarm@gmail.com>
2019-08-29 17:57:41 -0700

    updates

--- samples/lcd1602serial/lcd1602serial.ino
+++ samples/lcd1602serial/lcd1602serial.ino
@@ -1,5 +1,8 @@
 // -----------------------------------------------------------------------------
 /*
+ * When syncing with a clock, start 8 seconds before the top,
+ *  i.e. turn the power on at 52.
+
   SDA - A4
   SCL - A5
 */
@@ -14,14 +17,23 @@ LiquidCrystal_I2C lcd(0x27, 20, 4);
 int displayColumns = 16;
 
 int theCounter = 0;
+int theSyncCounter = 0;
 void displayOneSecondCount() {
   //
-  // when 993, Arduino is sligtly behind.
-  delay(992);
+  // If 992, Arduino is part of a second faster, in a thousand count.
+  // If 993, Arduino gets behind.
+  if (theSyncCounter == 1000) {
+    // Add a delay to get back in sync.
+    delay(996);
+    theSyncCounter = 0;
+  } else {
+    delay(993);
+  }
   //
   lcd.setCursor(8, 1);
   lcd.print(theCounter);
   theCounter++;
+  theSyncCounter++;
 }
 
 void displayPrintln(int theRow, String theString) {
@@ -41,14 +53,14 @@ void displayPrintln(int theRow, String theString) {
   lcd.setCursor(0, theRow);
   lcd.print(printString);
   /*  For testing.
-  Serial.print("+ theRest = ");
-  Serial.print(theRest);
-  Serial.print(", printString :");
-  Serial.print(printString);
-  Serial.print(":");
-  Serial.print(", theString :");
-  Serial.print(theString);
-  Serial.println(":");
+    Serial.print("+ theRest = ");
+    Serial.print(theRest);
+    Serial.print(", printString :");
+    Serial.print(printString);
+    Serial.print(":");
+    Serial.print(", theString :");
+    Serial.print(theString);
+    Serial.println(":");
   */
 }
 
@@ -65,13 +77,13 @@ void setup() {
   lcd.backlight();
   Serial.println("+ print: Hello there.");
   //                1234567890123456
-  displayPrintln(0,"Hello there.");
+  displayPrintln(0, "Hello there.");
   delay(3000);
   lcd.clear();
   //
   Serial.println("+ Start counting.");
   //                1234567890123456
-  displayPrintln(0,"Start counting.");
+  displayPrintln(0, "Start counting.");
   lcd.setCursor(1, 1);
   lcd.print("Count#");
   displayOneSecondCount();
