5d5788deaa9d64ffd8a4a3e15d1ede108eae4fa8
tigerfarm <tigerfarm@gmail.com>
2019-09-24 13:15:06 -0700

    updates

--- samples/pongv21/pongv21.ino
+++ samples/pongv21/pongv21.ino
@@ -8,15 +8,50 @@
    
    To compile, from the library manager (Sketch/Include library/Manage libraries), install: tvout.
    Move the directory, TVoutfonts, to the directory: Documents/Arduino/libraries.
+   Documentation: https://code.google.com/archive/p/arduino-tvout/wikis
+   + clear_screen() Clear the screen.
+   + hres() returns the horizontal resolution
+   + vres() returns the vertical resolution
+   + select_font(font): font4x6, font6x8, font8x8, font8x8ext
+   + print(x,y,string) or println(x,y,string)
+   + draw_line(x0,y0,x1,y1,color)
+   + set_pixel(x,y,color) Set a pixel.
+   + get_pixel(x,y) Get a pixel.
+   + delay_frame(1): To prevent screen flashes.
 
    To make the game more interesting to play:
     + User set game ball speed.
-    + Long wires (3 wire cable) for each gamer.
+    + Long wires (3 wire cable) for each controller.
 */
 #include <TVout.h>
 #include <fontALL.h>
 
 TVout TV;
+
+// -----------------------
+/*
+  Wiring:
+  + 2x Variable resisters: 10k ohm Potentiometers
+  + Variable resisters: left pin to v+
+  + Variable resisters: right pin to v-
+  + Nano A0 : variable resister, center pin: right paddle controller
+  + Nano A1 : variable resister, center pin: left paddle controller
+  + Nano D2 : button, pull down resister to v-, other button side to v+.
+  
+  Following pins are set by the TV library.
+  + Nano D7 : to 1K resister, to the RCA video cable signal wire (center wire).
+  + Nano D9 : to 470 ohm resister, to signal wire.
+  + RCA video cable shielding (ground) wire, to v-.
+  2nd RCA cable for sound.
+  + Wire the signal pin to D11 for TV tones (TV.tone).
+*/
+#define PLAYER_ONE_PIN 0 // analog paddle control: right paddle
+#define PLAYER_TWO_PIN 1 // analog paddle control: left paddle
+
+#define PADDLE_HEIGHT 14
+#define PADDLE_WIDTH 1
+#define RIGHT_PADDLE_X (TV.hres()-4)
+#define LEFT_PADDLE_X 2
 
 #define PLAY_TO 6
 #define MAX_Y_VELOCITY 12
@@ -30,14 +65,6 @@ TVout TV;
 #define BUTTON_ONE_PIN 2 // Control the game state
 
 int state = IN_MENU;
-
-// -----------------------
-#define PLAYER_ONE_PIN 0 // analog paddle control
-#define PLAYER_TWO_PIN 1 // analog paddle control
-#define PADDLE_HEIGHT 14
-#define PADDLE_WIDTH 1
-#define RIGHT_PADDLE_X (TV.hres()-4)
-#define LEFT_PADDLE_X 2
 
 // -----------------------
 // Display message locations
@@ -75,7 +102,7 @@ void drawMenu() {
   Serial.println("+ drawMenu");
   TV.clear_screen();
   TV.select_font(font8x8);
-  TV.print(6, 5, "Arduino Pong V2.1");
+  TV.print(6, 5, "Pong V2.1");
   drawGameStart();
 }
 void drawGameStart() {
@@ -107,23 +134,6 @@ void drawScore() {
   TV.print_char(RIGHT_SCORE_X, SCORE_Y, '0' + rightPlayerScore);
 }
 
-void buttonSetState() {
-  // Manage control for multiple button presses:
-  //  1. Start a new game in 1 second.
-  //  2. (to do) If pressed again, within 1 second, set faster ball speed.
-  //  3.1 If pressed while in a game, pause the game.
-  //  3.2 If pressed while in game pause state, continue the game.
-  if (state == PLAYING_GAME) {
-    state = PLAYING_GAME_PAUSE;
-    TV.select_font(font8x8);
-    TV.print(DISPLAY_MESSAGE_X, GAME_OVER_Y, "Game Paused.");
-  } else if (state == PLAYING_GAME_PAUSE) {
-    state = PLAYING_GAME;
-  } else if (state == IN_MENU || state == GAME_OVER) {
-    state = PLAYING_GAME;
-  }
-}
-
 // -----------------------------------------------------------------------
 // Game moving components
 
@@ -144,15 +154,23 @@ void drawMenuBall() {
     TV.delay_frame(2);
     delay(25);
     //
-    if (x + volX < 1 || x + volX > TV.hres() - 1) volX = -volX;
-    if (y + volY < 1 || y + volY > TV.vres() - 1) volY = -volY;
+    if (x + volX < 1 || x + volX > TV.hres() - 1) {
+      volX = -volX;
+      delay(100); TV.tone( 2000,30  );  // need to test tones.
+    }
+    if (y + volY < 1 || y + volY > TV.vres() - 1) {
+      volY = -volY;
+      delay(100); TV.tone( 2000,30  );
+    }
     if (TV.get_pixel(x + volX, y + volY)) {
       TV.set_pixel(x + volX, y + volY, 0);
       if (TV.get_pixel(x + volX, y - volY) == 0) {
         volY = -volY;
+        delay(100); TV.tone( 2000,30  );
       }
       else if (TV.get_pixel(x - volX, y + volY) == 0) {
         volX = -volX;
+        delay(100); TV.tone( 2000,30  );
       }
       else {
         volX = -volX;
@@ -191,9 +209,11 @@ void drawGameBall() {
   // Scoring
   if (ballX <= 1) {
     playerScored(RIGHT);
+    delay(100); TV.tone( 500,300 );
   }
   if (ballX >= TV.hres() - 1) {
     playerScored(LEFT);
+    delay(100); TV.tone( 500,300 );
   }
   //
   // draw the ball
@@ -228,32 +248,31 @@ void setup()  {
   Serial.println("+ Setup complete.");
 }
 
+void buttonSetState() {
+  // Manage control for multiple button presses:
+  //  + If pressed while in a game, pause the game.
+  //  + If pressed while in game pause state, continue the game.
+  //  + If in the menu, start a new game in 1 second.
+  //  ++ (to do) If pressed again, within 1 second, set faster ball speed.
+  if (state == PLAYING_GAME) {
+    state = PLAYING_GAME_PAUSE;
+    TV.select_font(font8x8);
+    TV.print(DISPLAY_MESSAGE_X, GAME_OVER_Y, "Game Paused.");
+  } else if (state == PLAYING_GAME_PAUSE) {
+    state = PLAYING_GAME;
+  } else if (state == IN_MENU) {
+    state = PLAYING_GAME;
+  } else if (state == GAME_OVER) {
+    state = PLAYING_GAME;
+  }
+}
+
 // -----------------------------------------------------------------------
 void loop() {
-
   paddleOnePosition = analogRead(PLAYER_ONE_PIN);
   paddleTwoPosition = analogRead(PLAYER_TWO_PIN);
   button1Status = (digitalRead(BUTTON_ONE_PIN));
-
   delay(25);
-
-  if (counter == 60) counter = 10; // increment or reset frame counter
-  counter++;
-  /*  For testing. However, when used, the TV output doesn't work.
-  Serial.print("+ loop counter = ");
-  Serial.print(counter);
-  Serial.print(" State: ");
-  Serial.print(state);
-  Serial.print("  Button Status Pressed PressedStill: ");
-  Serial.print(button1Status);
-  Serial.print(" ");
-  Serial.print(buttonPressed);
-  Serial.print(" Paddle One Two: ");
-  Serial.print(paddleOnePosition);
-  Serial.print(" ");
-  Serial.println(paddleTwoPosition);
-  */
-  
   // ---------------------------------------------------------------------
   if (button1Status) {
     // Button toggle to handle a quick click, or a click and hold.
@@ -268,6 +287,8 @@ void loop() {
   // ---------------------------------------------------------------------
   // Game state loop activities
   
+  if (counter == 60) counter = 10; // increment or reset frame counter
+  counter++;
   if (state == PLAYING_GAME) {
 
     if (counter % 2 == 0) {
@@ -285,6 +306,21 @@ void loop() {
     drawMenuBall();
   }
 
+  /*  For testing. However, when used, the TV output doesn't work.
+  Serial.print("+ loop counter = ");
+  Serial.print(counter);
+  Serial.print(" State: ");
+  Serial.print(state);
+  Serial.print("  Button Status Pressed PressedStill: ");
+  Serial.print(button1Status);
+  Serial.print(" ");
+  Serial.print(buttonPressed);
+  Serial.print(" Paddle One Two: ");
+  Serial.print(paddleOnePosition);
+  Serial.print(" ");
+  Serial.println(paddleTwoPosition);
+  */
+  
 } // loop end
 // -----------------------------------------------------------------------
 // eof
