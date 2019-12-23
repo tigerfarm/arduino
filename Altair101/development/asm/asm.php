e1421405ff1c98882ef59d84a8dcd535f17fad23
tigerfarm <tigerfarm@gmail.com>
2019-12-23 11:48:33 -0800

    updates

---
+++ Altair101/development/asm/asm.php
@@ -0,0 +1,46 @@
+<?php
+
+// -----------------------------------------------------------------------------
+// PHP string functions: 
+// https://www.w3schools.com/php/php_ref_string.asp
+
+function startsWith($string, $startString) {
+    $len = strlen($startString);
+    return (substr($string, 0, $len) === $startString);
+}
+
+function parseLine($lineParts) {
+    $arrayLength = count($lineParts);
+    echo "Array length = " . $arrayLength . " ";
+    if ($lineParts[0] == "mvi") {
+        echo "MVI";
+    }
+    echo "|";
+    for ($i = 0; $i < $arrayLength; $i++) {
+        echo $lineParts[$i] . "|";
+    }
+}
+
+// -----------------------------------------------------------------------------
+echo "+++ Start.\xA";
+
+$asmFile = "p1.asm";
+echo "+ Read file: " . $asmFile . "\xA";
+$readFile = fopen($asmFile, "r");
+while (!feof($readFile)) {
+    $lineParts = explode(" ", trim(fgets($readFile)));
+    echo "++ ";
+    if ($lineParts[0] == "") {
+        echo "Empty line.";
+    } elseif (startsWith($lineParts[0], ";")) {
+        echo "Comment line.";
+    } else {
+        parseLine($lineParts);
+    }
+    echo "\xA";
+}
+fclose($readFile);
+echo "+ File read, and closed.\xA";
+
+echo "\xA+++ Exit.\xA";
+?>
---
+++ Altair101/development/asm/p1.asm
@@ -0,0 +1,6 @@
+; Sample test program.
+org 0
+
+Start:
+mvi a,1
+mvi b,2
