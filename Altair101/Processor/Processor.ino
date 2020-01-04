59dac5e9083bb5ae260d68e78c09779b89af7689
tigerfarm <tigerfarm@gmail.com>
2020-01-03 22:43:07 -0800

    updates

--- Altair101/Processor/Processor.ino
+++ Altair101/Processor/Processor.ino
@@ -93,6 +93,7 @@
 // -----------------------------------------------------------------------------
 // Code compilation options.
 
+#define INCLUDE_CLOCK 1
 // #define INCLUDE_LCD 1
 // #define INCLUDE_SDCARD 1
 // #define RUN_DELAY 1
@@ -127,7 +128,8 @@ void pcf20interrupt() {
   pcf20interrupted = true;
 }
 
-// ------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
 // Output LED light shift register(SN74HC595N) pins
 
 //                Nano pin               74HC595 Pins
@@ -3345,6 +3347,174 @@ void infraredRunning() {
 
 // -----------------------------------------------------------------------------
 // -----------------------------------------------------------------------------
+// Clock
+
+#ifdef INCLUDE_CLOCK
+
+// For the clock board.
+#include "RTClib.h"
+RTC_DS3231 rtc;
+DateTime now;
+
+int theCounterHours = 0;
+int theCounterMinutes = 0;
+int theCounterSeconds = 0;
+
+void syncCountWithClock() {
+  now = rtc.now();
+  theCounterHours = now.hour();
+  theCounterMinutes = now.minute();
+  theCounterSeconds = now.second();
+  //
+  Serial.print("+ syncCountWithClock,");
+  Serial.print(" theCounterHours=");
+  Serial.print(theCounterHours);
+  Serial.print(" theCounterMinutes=");
+  Serial.print(theCounterMinutes);
+  Serial.print(" theCounterSeconds=");
+  Serial.println(theCounterSeconds);
+}
+
+// -----------------------------------------------------------------------------
+void processClockNow() {
+  //
+  now = rtc.now();
+  //
+  if (now.second() != theCounterSeconds) {
+    // When the clock second value changes, that's a second pulse.
+    theCounterSeconds = now.second();
+    // clockPulseSecond();
+    if (theCounterSeconds == 0) {
+      // When the clock second value changes to zero, that's a minute pulse.
+      theCounterMinutes = now.minute();
+      if (theCounterMinutes != 0) {
+        // clockPulseMinute();
+        Serial.print("+ clockPulseMinute(), theCounterMinutes= ");
+        Serial.println(theCounterMinutes);
+        // ----------------------------------------------
+        displayTheTime( theCounterMinutes, theCounterHours );
+      } else {
+        // When the clock minute value changes to zero, that's an hour pulse.
+        // clockPulseHour();
+        theCounterHours = now.hour();
+        Serial.print("++ clockPulseHour(), theCounterHours= ");
+        Serial.println(theCounterHours);
+        // ----------------------------------------------
+        displayTheTime( theCounterMinutes, theCounterHours );
+      }
+    }
+  }
+}
+
+// ------------------------------------------------------------------------
+// Display hours and minutes on LED lights.
+
+void displayTheTime(byte theMinute, byte theHour) {
+  byte theMinuteOnes = 0;
+  byte theMinuteTens = 0;
+  byte theBinaryMinute = 0;
+  byte theBinaryHour1 = 0;
+  byte theBinaryHour2 = 0;
+
+  // ----------------------------------------------
+  // Convert the minute into binary for display.
+  if (theMinute < 10) {
+    theBinaryMinute = theMinute;
+  } else {
+    // There are 3 bits for the tens: 0 ... 5 (00, 10, 20, 30, 40, or 50).
+    // There are 4 bits for the ones: 0 ... 9.
+    // LED diplay lights: ttt mmmm
+    // Example:      23 = 010 0011
+    //      Clock: Tens(t) & Minutes(m): B-tttmmmm
+    //                                   B00001111 = 2 ^ 4 = 16
+    // Altair 101: Tens(t) & Minutes(m): Bttt-mmmm
+    //                                   B00011111 = 2 ^ 5 = 32
+    // theMinute = 10, theBinaryMinute =  00100000
+    theMinuteTens = theMinute / 10;
+    theMinuteOnes = theMinute - theMinuteTens * 10;
+    theBinaryMinute = 32 * theMinuteTens + theMinuteOnes;
+  }
+
+  // ----------------------------------------------
+  // Convert the hour into binary for display.
+  // Use a 12 hour clock value rather than 24 value.
+  if (theHour > 12) {
+    theHour = theHour - 12;
+  } else if (theHour == 0) {
+    theHour = 12; // 12 midnight, 12am
+  }
+  switch (theHour) {
+    case 1:
+      theBinaryHour1 = B00000001; // Note, on the shift register, B00000001 is not wired, not used.
+      theBinaryHour2 = 0;
+      break;
+    case 2:
+      theBinaryHour1 = B00000010;
+      theBinaryHour2 = 0;
+      break;
+    case 3:
+      theBinaryHour1 = B00000100;
+      theBinaryHour2 = 0;
+      break;
+    case 4:
+      theBinaryHour1 = B00001000;
+      theBinaryHour2 = 0;
+      break;
+    case 5:
+      theBinaryHour1 = B00010000;
+      theBinaryHour2 = 0;
+      break;
+    case 6:
+      theBinaryHour1 = B00100000;
+      theBinaryHour2 = 0;
+      break;
+    case 7:
+      theBinaryHour1 = B01000000;
+      theBinaryHour2 = 0;
+      break;
+    case 8:
+      theBinaryHour1 = B10000000;
+      theBinaryHour2 = 0;
+      break;
+    case 9:
+      theBinaryHour1 = 0;
+      theBinaryHour2 = B00000001;
+      break;
+    case 10:
+      theBinaryHour1 = 0;
+      theBinaryHour2 = B00000010;
+      break;
+    case 11:
+      theBinaryHour1 = 0;
+      theBinaryHour2 = B00000100;
+      break;
+    case 12:
+      theBinaryHour1 = 0;
+      theBinaryHour2 = B00001000;
+      break;
+  }
+  // ----------------------------------------------
+  // void lightsStatusAddressData( byte status8bits, unsigned int address16bits, byte data8bits) { ... }
+  // lightsStatusAddressData(statusByte, programCounter, dataByte);
+  //
+  // Need enter: theBinaryMinute, theBinaryHour1, theBinaryHour2, into the following:
+  // Data byte LED lights: theBinaryMinute.
+  // Address word LED lights: theBinaryHour2 & theBinaryHour1.
+  unsigned int hourWord = theBinaryHour2 * 256 + theBinaryHour1;
+  lightsStatusAddressData(statusByte, hourWord, theBinaryMinute);
+
+  /*
+// Loop for displaying hours and minutes.
+void loop() {
+  delay(100);
+  processClockNow();
+}
+   */
+}
+#endif
+
+// -----------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
 void setup() {
   Serial.begin(115200);
   delay(1000);        // Give the serial connection time to start before the first print.
@@ -3399,8 +3569,20 @@ void setup() {
   int testLights = B10011001 * 256 + B01100110;
   lightsStatusAddressData(statusByte, testLights, dataByte);
   Serial.println(F("+ Front panel LED lights initialized."));
+  
   // ----------------------------------------------------
-
+  // Initialize the Real Time Clock (RTC).
+#ifdef INCLUDE_CLOCK
+  if (!rtc.begin()) {
+    Serial.println("--- Error: RTC not found.");
+    while (1);
+  }
+  syncCountWithClock();
+  displayTheTime( theCounterMinutes, theCounterHours );
+  Serial.println("+ Clock set and synched with program variables.");
+#endif
+  
+  // ----------------------------------------------------
   Serial.println(F("+++ Start the processor loop."));
 }
 
