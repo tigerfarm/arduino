d450aec3c3490ef1fe01a776397efdf0faa40ab9
tigerfarm <tigerfarm@gmail.com>
2020-01-11 09:34:10 -0800

    updates

--- Altair101/development/asm/programs/pKillTheBit.asm
+++ Altair101/development/asm/programs/pKillTheBit.asm
@@ -1,15 +1,20 @@
+                        ; ------------------------------------------------
+                        ; Kill the Bit program.
+                        ; Reference online video:
+                        ; https://www.youtube.com/watch?v=ZKeiQ8e18QY&list=PLLlz7OhtlfKYk8nkyF1u-cDwzE_S0vcJs&index=5
+                        ; 
+                        ; ------------------------------------------------
             org 0       ; An assembler directive on where to load this program.
-            ; nop
     Start:
             LXI H,0     ; Move the lb hb data values into the register pair H(hb):L(lb). Initialize counter
             mvi D,080h  ; Move db to register D. Set initial display bit.  080h = 0200 = regD = 10 000 000
                         ; Fix: Change 080h to 0x80 with is decimal 128.
             LXI B,500h  ; Load a(lb:hb) into register B:C. Higher value = faster. Default: 0016 = B:C  = 00 010 000
-                        ; Fix: B00000001, 0, 5,     //   6: lxi b,5
+                        ; Fix: B00000001, 0, 5,     //   5: lxi b,500h
                         ;  My default: 0005 = B:C  = 00 000 101
                         ;     Default: 0014 = B:C  = 00 010 000
                         ;     Slow:    0020 = B:C  = 00 010 000
-                        ;     Nice:    0040 = B:C  = 00 100 000
+                        ;     Nice:    0040 = B:C  = 00 100 000 Similar to the video speed. They use: 016h, I use
                         ;     Fast:    0100 = B:C  = 01 000 000
                         ; Too fast:    0100 = B:C  = 01 001 000
                         ; ------------------------------------------------
