eb99ed1c5905f1230bca59b206b268d073b95f59
tigerfarm <tigerfarm@gmail.com>
2020-01-22 18:53:35 -0800

    updates

--- Altair101/asm/src/asm/opcodeInfo.java
+++ Altair101/asm/src/asm/opcodeInfo.java
@@ -3,56 +3,79 @@ package asm;
 import java.util.Arrays;
 import java.util.Comparator;
 
+// -----------------------------------------------------------------------------
 public class opcodeInfo {
 
-    int rollno;
-    String name, address;
+    int value;
+    String name, info;
 
     // Constructor 
-    public opcodeInfo(int rollno, String name, String address) {
-        this.rollno = rollno;
+    public opcodeInfo(byte value, String name, String info) {
+        this.value = value;
         this.name = name;
-        this.address = address;
+        this.info = info;
     }
 
-    // Used to print student details in main() 
+    @Override
     public String toString() {
-        return this.rollno + " " + this.name
-                + " " + this.address;
+        return byteToString((byte)this.value)
+                + " " + this.name
+                + ": " + this.info;
     }
+
+    // -------------------------------------------------------------------------
+    public static String byteToString(byte aByte) {
+        return toBinary(aByte, 8);
+    }
+
+    private static String toBinary(byte a, int bits) {
+        if (--bits > 0) {
+            return toBinary((byte) (a >> 1), bits) + ((a & 0x1) == 0 ? "0" : "1");
+        } else {
+            return (a & 0x1) == 0 ? "0" : "1";
+        }
+    }
+
 }
 
-class Sortbyroll implements Comparator<opcodeInfo> {
+class SortbyValue implements Comparator<opcodeInfo> {
+    @Override
     public int compare(opcodeInfo a, opcodeInfo b) {
-        return a.rollno - b.rollno;
+        return a.value - b.value;
     }
 }
 
-class Sortbyname implements Comparator<opcodeInfo> {
+class SortbyName implements Comparator<opcodeInfo> {
+    @Override
     public int compare(opcodeInfo a, opcodeInfo b) {
         return a.name.compareTo(b.name);
     }
 }
 
+// -----------------------------------------------------------------------------
+// For testing.
+
 class Main {
 
     public static void main(String[] args) {
+        
+        // Need a method to load the data from a text file.
         opcodeInfo[] arr = {
-            new opcodeInfo(111, "bbbb", "london"),
-            new opcodeInfo(131, "aaaa", "nyc"),
-            new opcodeInfo(121, "cccc", "jaipur")
+            new opcodeInfo((byte)0b11000110, "adi", "ADI #    11 000 110  3  Add immediate number to register A, set: ZSCPA."),
+            new opcodeInfo((byte)0b11100110, "ani", "ANI #    11 100 110  2  AND # (immediate db) with register A."),
+            new opcodeInfo((byte)0b11001101, "call", "CALL a   11 001 101  3  Unconditional subroutine call. Push current address onto the stack and jump the subroutine address.")
         };
 
         System.out.println("Unsorted");
         for (int i = 0; i < arr.length; i++) {
             System.out.println(arr[i]);
         }
-        Arrays.sort(arr, new Sortbyroll());
-        System.out.println("\nSorted by rollno");
+        Arrays.sort(arr, new SortbyValue());
+        System.out.println("\nSorted by value");
         for (int i = 0; i < arr.length; i++) {
             System.out.println(arr[i]);
         }
-        Arrays.sort(arr, new Sortbyname());
+        Arrays.sort(arr, new SortbyName());
         System.out.println("\nSorted by name");
         for (int i = 0; i < arr.length; i++) {
             System.out.println(arr[i]);
