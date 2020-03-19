fc439ebe9de7f081a171fefe1becadfba670e323
tigerfarm <tigerfarm@gmail.com>
2020-03-19 14:43:39 -0700

    updates

--- instructables/infraredReceiverTest/infraredReceiverTest.ino
+++ instructables/infraredReceiverTest/infraredReceiverTest.ino
@@ -1,8 +1,8 @@
 // -----------------------------------------------------------------------------
 /*
-    Test DS3231 clock module.
+  Test infrared receiver.
 
-    + Post messages to the serial port, which can be displayed using the Arduino Tools/Serial Monitor.
+  + Post messages to the serial port, which can be displayed using the Arduino Tools/Serial Monitor.
 
   Connect infrared receiver, pins from top left to right:
     Left most (next to the X) - Nano pin 9
@@ -21,24 +21,111 @@
   ---------
 
   ------------------------------------------------------------------------------
-  DS3231 Clock Library:
-    Filter your search by typing ‘rtclib’.
-    There should be a couple entries. Look for RTClib by Adafruit.
-  https://github.com/adafruit/RTClib
+  Install library: IRremote by Shirriff, I installed version: 2.2.3
 */
-// -----------------------------------------------------------------------
+// -----------------------------------------------------------------------------
 // For the infrared receiver.
 #include <IRremote.h>
 int IR_PIN = A1;
 IRrecv irrecv(IR_PIN);
 decode_results results;
 
+void infraredSwitch() {
+  // Serial.println("+ infraredSwitch");
+  switch (results.value) {
+    case 0xFFFFFFFF:
+      // Ignore. This is from holding the key down.
+      break;
+    // -----------------------------------
+    case 0xFF10EF:
+    case 0xE0E0A659:
+      Serial.println("+ Key < - previous");
+      break;
+    case 0xFF5AA5:
+    case 0xE0E046B9:
+      Serial.println("+ Key > - next");
+      break;
+    case 0xFF18E7:
+    case 0xE0E006F9:
+      Serial.println("+ Key up");
+      break;
+    case 0xFF4AB5:
+    case 0xE0E08679:
+      Serial.println("+ Key down");
+      break;
+    case 0xFF38C7:
+    case 0xE0E016E9:
+      Serial.println("+ Key OK - Toggle");
+      break;
+    // -----------------------------------
+    case 0xFF9867:
+    case 0xE0E08877:
+      Serial.print("+ Key 0:");
+      Serial.println("");
+      break;
+    case 0xFFA25D:
+      Serial.print("+ Key 1: ");
+      Serial.println("");
+      break;
+    case 0xFF629D:
+      Serial.print("+ Key 2: ");
+      Serial.println("");
+      break;
+    case 0xFFE21D:
+      Serial.print("+ Key 3: ");
+      Serial.println("");
+      break;
+    case 0xFF22DD:
+      Serial.print("+ Key 4: ");
+      Serial.println("");
+      break;
+    case 0xFF02FD:
+      Serial.print("+ Key 5: ");
+      Serial.println("");
+      break;
+    case 0xFFC23D:
+      Serial.print("+ Key 6: ");
+      Serial.println("");
+      break;
+    case 0xFFE01F:
+      Serial.print("+ Key 7: ");
+      Serial.println("");
+      break;
+    case 0xFFA857:
+      Serial.print("+ Key 8: ");
+      Serial.println("");
+      break;
+    case 0xFF906F:
+      Serial.print("+ Key 9: ");
+      Serial.println("");
+      break;
+    // -----------------------------------
+    case 0xFF6897:
+    case 0xE0E01AE5:
+      Serial.println("+ Key * (Return)");
+      break;
+    case 0xFFB04F:
+    case 0xE0E0B44B:
+      Serial.println("+ Key # (Exit)");
+      break;
+    // -----------------------------------
+    default:
+      Serial.print("+ Result value: ");
+      Serial.println(results.value, HEX);
+      // -----------------------------------
+  } // end switch
+
+}
+
+// -----------------------------------------------------------------------
 void setup() {
   Serial.begin(115200);
   // Give the serial connection time to start before the first print.
   delay(1000);
   Serial.println("+++ Setup.");
 
+  irrecv.enableIRIn();
+  Serial.println("+ Initialized the infrared receiver.");
 
   Serial.println("++ Go to loop.");
 }
@@ -46,11 +133,13 @@ void setup() {
 // -----------------------------------------------------------------------------
 // Device Loop
 
-int counter = 0;
-boolean ledOn = false;
 void loop() {
-  delay(1000);
-  Serial.print("+ Loop counter = ");
-  Serial.println(counter);
+  delay(60);
+  // ---------------------------------------------------------------------
+  // Process infrared key presses.
+  if (irrecv.decode(&results)) {
+    infraredSwitch();
+    irrecv.resume();
+  }
 }
 // -----------------------------------------------------------------------------
