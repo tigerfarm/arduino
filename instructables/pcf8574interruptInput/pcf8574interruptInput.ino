b8b2c1232d83794d57d068757a191eecd4a107f8
tigerfarm <tigerfarm@gmail.com>
2020-04-18 11:26:14 -0700

    updates

--- instructables/pcf8574interruptInput/pcf8574interruptInput.ino
+++ instructables/pcf8574interruptInput/pcf8574interruptInput.ino
@@ -24,7 +24,7 @@
   | P5           |
   | P6           |
   | P7           |
-  | INT          | Interrupt pin is not used in this program.
+  | INT          | For multiple, test using a diode to the Arduino interrupt pin.
   |              |
   |  V G S S     | Below are male pins
   |  C N D C     |
@@ -37,7 +37,11 @@
   + SCL to Arduino A5.
   + GND to Arduino GND
   + VCC to Arduino 5V
+  + INT to Arduino interrupt pin such as pin 2 in this sample program.
   + P0 ... O7 to switches. Other side of the switch to ground.
+
+  When using multiple modules to the same Arduino interrupt pin,
+  + Test with a diode from the modules to the Arduino interrupt pin.
 
 */
 // -----------------------------------------------------------------------------
@@ -65,9 +69,93 @@ void printByte(byte b) {
 // PCF8574 pcfSwitches(0x022);   // High bytes
 PCF8574 pcfSwitches(0x023);    // AUX switches and others: Step down, CLR, Protect, Unprotect, AUX1 up, AUX1 down,  AUX2 up, AUX2 down
 
+// -------------------------
+// Interrupt handler routine.
+//                   Mega pin for control toggle interrupt. Same pin for Nano.
+const int INTERRUPT_PIN = 2;
+
+boolean pcfSwitchesinterrupted = false;
+void pcfSwitchesinterrupt() {
+  pcfSwitchesinterrupted = true;
+}
+
 // -----------------------------------------------------------------------------
 // -----------------------------------------------------------------------------
 // Front Panel Switches
+
+// --------------------------------------------------------
+// Front Panel Control Switches, when a program is not running.
+// Switches: AUX 1, AUX 1, PROTECT, UNPROTECT, CLR, and down STEP.
+
+const int pcfSwitch0 = 0;
+const int pcfSwitch1 = 1;
+const int pcfSwitch2 = 2;
+const int pcfSwitch3 = 3;
+const int pcfSwitch4 = 4;
+const int pcfSwitch5 = 5;
+const int pcfSwitch6 = 6;
+const int pcfSwitch7 = 7;
+
+boolean switchLogic0 = false;
+boolean switchLogic1 = false;
+boolean switchLogic3 = false;
+boolean switchLogic4 = false;
+
+void checkSwitches() {
+  // -------------------
+  if (pcfSwitches.readButton(pcfSwitch0) == 0) {
+    if (!switchLogic0) {
+      switchLogic0 = true;
+    }
+  } else if (switchLogic0) {
+    switchLogic0 = false;
+    // Switch logic.
+    Serial.println(F("+ Control, pcfSwitch0."));
+  }
+  // -------------------
+  if (pcfSwitches.readButton(pcfSwitch1) == 0) {
+    if (!switchLogic1) {
+      switchLogic1 = true;
+    }
+  } else if (switchLogic1) {
+    switchLogic1 = false;
+    // Switch logic.
+    Serial.println(F("+ Control, pcfSwitch1."));
+  }
+  // -------------------
+  if (pcfSwitches.readButton(pcfSwitch2) == 0) {
+    if (!switchLogic3) {
+      switchLogic3 = true;
+    }
+  } else if (switchLogic3) {
+    switchLogic3 = false;
+    // Switch logic.
+    Serial.println(F("+ Control, pcfSwitch2."));
+  }
+  // -------------------
+  if (pcfSwitches.readButton(pcfSwitch3) == 0) {
+    if (!switchLogic1) {
+      switchLogic1 = true;
+    }
+  } else if (switchLogic1) {
+    switchLogic1 = false;
+    // Switch logic.
+    Serial.println(F("+ Control, pcfSwitch3."));
+  }
+  // -------------------
+  if (pcfSwitches.readButton(pcfSwitch4) == 0) {
+    Serial.println(F("+ Control, pcfSwitch4."));
+  }
+  if (pcfSwitches.readButton(pcfSwitch5) == 0) {
+    Serial.println(F("+ Control, pcfSwitch5."));
+  }
+  if (pcfSwitches.readButton(pcfSwitch6) == 0) {
+    Serial.println(F("+ Control, pcfSwitch6."));
+  }
+  if (pcfSwitches.readButton(pcfSwitch7) == 0) {
+    Serial.println(F("+ Control, pcfSwitch7."));
+  }
+}
 
 // -------------------------
 // Getting a byte of toggle values.
@@ -112,8 +200,11 @@ void setup() {
   Serial.println("+++ Setup.");
 
   // ------------------------------
+  // I2C Two Wire + interrupt initialization
   pcfSwitches.begin();
-  Serial.println("+ PCF PCF8574 I2C Two Wire module initialized.");
+  pinMode(INTERRUPT_PIN, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), pcfSwitchesinterrupt, CHANGE);
+  Serial.println("+ PCF module initialized.");
 
   // ------------------------------
   Serial.println("+++ Go to loop.");
@@ -124,6 +215,15 @@ void setup() {
 
 int counter = 0;
 void loop() {
+
+  if (pcfSwitchesinterrupted) {
+    Serial.println("---------------------------");
+    Serial.println("+ Interrupt call, switchSetOn is true.");
+    echoSwitchData();
+    checkSwitches();
+    pcfSwitchesinterrupted = false; // Reset for next interrupt.
+  }
+
   delay (50);
   counter++;
   // 20 is 1 second (20 x 50 = 1000). 40 is every 2 seconds.
