598b43c97c4cb3b9590eb2b3064bd6cade81ebb9
tigerfarm <tigerfarm@gmail.com>
2020-04-21 15:06:03 -0700

    updates

--- Altair101/development/74HC595altair/74HC595altair.ino
+++ Altair101/development/74HC595altair/74HC595altair.ino
@@ -1,7 +1,8 @@
 // -----------------------------------------------------------------------------
 /*
-  Testing LED light implementations for both the Tablet and the Desktop modules.
-
+  Testing front panel LED light implementations for both the Tablet and the Desktop modules.
+  + Status, data, and address lights.
+  + Does not include WAIT and HLDA, thoses are tested in the PCF toggle program.
 */
 // -----------------------------------------------------------------------------
 // If defined, Desktop module. Else Tablet module.
@@ -21,14 +22,36 @@ const int dataPinLed  = A14;    // pin 14 Data pin.
 const int latchPinLed = A12;    // pin 12 Latch pin.
 const int clockPinLed = A11;    // pin 11 Clock pin.
 
-// Status lights: INT, WO, STACK, HLTA, OUT, MI, INP, MEMR
 byte statusByte = B00000000;        // By default, all are OFF.
 byte dataByte = B01010101;
 unsigned int curProgramCounter = 0;     // Current program address value
 
-unsigned int addressWord;
+// -----------------------------------------------------------------------------
+// Status lights: INT, WO, STACK, HLTA, OUT, MI, INP, MEMR
 
-// ------------------------------------------------
+// Use OR to turn ON. Example:
+//  statusByte = statusByte | MEMR_ON;
+const byte MEMR_ON =    B10000000;  // MEMR   The memory bus will be used for memory read data.
+const byte INP_ON =     B01000000;  // INP    The address bus containing the address of an input device. The input data should be placed on the data bus when the data bus is in the input mode
+const byte M1_ON =      B00100000;  // M1     Machine cycle 1, fetch opcode.
+const byte OUT_ON =     B00010000;  // OUT    The address contains the address of an output device and the data bus will contain the out- put data when the CPU is ready.
+const byte HLTA_ON =    B00001000;  // HLTA   Machine opcode hlt, has halted the machine.
+const byte STACK_ON =   B00000100;  // STACK  Stack process
+const byte WO_ON =      B00000010;  // WO     Write out (inverse logic)
+const byte INT_ON =     B00000001;  // INT    Interrupt
+
+// Use AND to turn OFF. Example:
+//  statusByte = statusByte & M1_OFF;
+const byte MEMR_OFF =   B01111111;
+const byte INP_OFF =    B10111111;
+const byte M1_OFF =     B11011111;
+const byte OUT_OFF =    B11101111;
+const byte HLTA_OFF =   B11110111;
+const byte STACK_OFF =  B11111011;
+const byte WO_OFF =     B11111101;
+const byte INT_OFF =    B11111110;
+
+// -----------------------------------------------------------------------------
 // Data LED lights displayed using shift registers.
 
 void processDataLights() {
@@ -83,12 +106,19 @@ void setup() {
   delay(3000);
   //
   // Turns all the LEDs on.
-  addressWord = B11111111 + B11111111 * 256;
-  lightsStatusAddressData(B11111111, addressWord, B11111111);
+  curProgramCounter = B11111111 + B11111111 * 256;
+  lightsStatusAddressData(B11111111, curProgramCounter, B11111111);
   delay(3000);
   Serial.print("+ Value to turn all Address LED lights on: ");
-  Serial.println(addressWord);
-  //
+  Serial.println(curProgramCounter);
+
+  // A counter in each section: 1) Status lights, 2) lower byte address, 3) Higher byte address, 4) Data byte
+  lightsStatusAddressData(0, 0, 0);
+  // 1, 2, 3, 4.
+  curProgramCounter = 3 +  4 * 256;
+  lightsStatusAddressData(1, curProgramCounter, 2);
+  delay(6000);
+
   // Turns all the LEDs off.
   // lightsStatusAddressData(0, 0, 0);
   statusByte = 0;
@@ -98,17 +128,97 @@ void setup() {
   delay(1000);
   //
   // Every other LED.
-  // lightsStatusAddressData(B01010101, addressWord, B01010101);
+  // lightsStatusAddressData(B01010101, curProgramCounter, B01010101);
   statusByte = B01010101;
   dataByte = B01010101;
   curProgramCounter = B01010101 + B01010101 * 256;
   processDataLights();
   delay(3000);
 
-  // 1, 2, 3, 4.
-  addressWord = 3 +  4 * 256;
-  lightsStatusAddressData(1, addressWord, 2);
+  // -------------------------
+  // To test, status lights.
+  statusByte = 0;
+  dataByte = 0;
+  curProgramCounter = 0;
+  processDataLights();
+  // -------------------------
+  statusByte = 0;
+  statusByte = MEMR_ON;
+  processDataLights();
+  delay(3000);
+  // -------------------------
+  statusByte = 0;
+  statusByte = INP_ON;
+  processDataLights();
+  delay(3000);
+  // -------------------------
+  statusByte = 0;
+  statusByte = M1_ON;
+  processDataLights();
+  delay(3000);
+  // -------------------------
+  statusByte = 0;
+  statusByte = OUT_ON;
+  processDataLights();
+  delay(3000);
+  // -------------------------
+  statusByte = 0;
+  statusByte = HLTA_ON;
+  processDataLights();
+  delay(3000);
+  // -------------------------
+  statusByte = 0;
+  statusByte = STACK_ON;
+  processDataLights();
+  delay(3000);
+  // -------------------------
+  statusByte = 0;
+  statusByte = WO_ON;
+  processDataLights();
+  delay(3000);
+  // -------------------------
+  statusByte = 0;
+  statusByte = INT_ON;
+  processDataLights();
+  delay(3000);
+  // -------------------------
+
+  statusByte = 0;
+  statusByte = statusByte | MEMR_ON;
+  statusByte = statusByte | M1_ON;
+  statusByte = statusByte | WO_ON;
+  processDataLights();
   delay(6000);
+  //
+  statusByte = statusByte & M1_OFF;
+  processDataLights();
+  delay(6000);
+  //
+  statusByte = MEMR_ON | WO_ON;
+  processDataLights();
+  delay(6000);
+  //
+  statusByte = 0;
+  statusByte = (MEMR_ON | WO_ON) & M1_OFF;
+  processDataLights();
+  delay(6000);
+
+  // -------------------------------------------------------------
+  // Move LED on light, from left to right.
+  statusByte = B01010101;
+  dataByte = B01010101;
+  curProgramCounter = B01010101 + B01010101 * 256;
+  lightsStatusAddressData(0, 0, 0); // Turns all the LEDs off.
+  delay(500);
+  statusByte = 1;
+  dataByte = 1;
+  curProgramCounter = 1 + 1 * 256;
+  for (int numberToDisplay = 0; numberToDisplay < 7; numberToDisplay++) {
+    dataByte = dataByte << 1;
+    curProgramCounter = dataByte +  dataByte * 256;
+    lightsStatusAddressData(dataByte, curProgramCounter, dataByte);
+    delay(500);
+  }
 
   Serial.println("+++ Start program loop.");
 }
@@ -122,10 +232,10 @@ void loop() {
   lightsStatusAddressData(0, 0, 0); // Turns all the LEDs off.
   delay(500);
   for (int numberToDisplay = 0; numberToDisplay < 256; numberToDisplay++) {
-    // Counts up: 0, 1, 2, 3, ..., 255
-    addressWord = numberToDisplay +  numberToDisplay * 256;
-    lightsStatusAddressData(numberToDisplay, addressWord, numberToDisplay);
-    delay(60);
+    // Counts up from 0 (no lights on), 1, 2, 3, ..., to 255 (all lights on).
+    curProgramCounter = numberToDisplay +  numberToDisplay * 256;
+    lightsStatusAddressData(numberToDisplay, curProgramCounter, numberToDisplay);
+    delay(100);
   }
 }
 // -----------------------------------------------------------------------------
