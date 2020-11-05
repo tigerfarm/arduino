72e1f4ed61391257afa577744133ba6259e1b22d
tigerfarm <tigerfarm@gmail.com>
2020-11-04 17:11:34 -0800

    updates

--- Altair101/asm/programs/opShld.asm
+++ Altair101/asm/programs/opShld.asm
@@ -1,37 +1,43 @@
                                     ; --------------------------------------
                                     ; Test SHLD.
+                                    ;
+                                    ; Load the H:L register pair bytes into a memory address and the memory address plus one.
+                                    ;
                                     ; SHLD adr : L -> (adr); H -> (adr+1)
-                                    ; Store data value from memory location: register L to a(address hb:lb).
-                                    ; Store data value from memory location: register H to a+1.
+                                    ; Store register L to address: adr(hb:lb).
+                                    ; Store register H to address: adr+1.
                                     ;
                                     ; --------------------------------------
     Start:
                 mvi a,0             ; Initialize the test counter.
                 sta testCounter
                                     ; --------------------------------------
-                lda byte1           ; Load register A from the address(hb:lb).
-                out 37              ; Print register A.
-                lda byte2           ; Load register A from the address(hb:lb).
-                out 37              ; Print register A.
-                                    ;
                 mvi h,0             ; Set address register pair H:L, to 0:0.
                 mvi l,0
                 out 36              ; Print the register values for H:L, and the content at that address.
                                     ;
+                sta byteL           ; Initialize the address values to zero.
+                sta byteH
+                lda byteL           ; Load and print the address values.
+                out 37
+                lda byteH
+                out 37
+                                    ;
                                     ; --------------------------------------
                 call NewTest
+                                    ;
                 lxi h,Addr3         ; Load the address value of Addr3 into H:L.
                                     ; Register H:L = 3:18, address# 786.
                 out 36              ; Print the register values for H:L, and the content at that address.
                                     ;
-                call Separator
-                shld byte1          ; Given byte1 address(hb:lb),
-                                    ;   Store register L to memory location of byte1.
-                                    ;   Store register H to memory location of byte1 + 1.
-                lda byte1           ; Load byte1 into register A from the memory location of byte1.
-                out 37              ; Print register A.
-                lda byte2           ; Load byte2 into register A from the memory location of byte1 + 1 which is byte2.
-                out 37              ; Print register A.
+                shld byteL          ; Given byteL address(hb:lb),
+                                    ;   Store register L to memory location of byteL (byteL address).
+                                    ;   Store register H to memory location of byteL + 1 (byteH address).
+                                    ;
+                lda byteL           ; Load and print the address values.
+                out 37
+                lda byteH
+                out 37
                                     ;
                                     ; --------------------------------------
                 hlt
@@ -102,19 +108,18 @@
     Addr1       equ     128         ; 0000 0000 1000 0000 H:L = 0:128
     Addr2       equ     512         ; 0000 0010 0000 0000 H:L = 2:000
     Addr3       equ     786         ; 0000 0011 0001 0010 H:L = 3:018
-    byte1       ds      1
-    byte2       ds      1
+    byteL       ds      1
+    byteH       ds      1
                                     ; --------------------------------------
                 end
                                     ; --------------------------------------
                                     ; Successful run:
                                     ;
- > Register A =   0 = 000 = 00000000
- > Register A =   0 = 000 = 00000000
  > Register H:L =   0:  0, Data:  62 = 076 = 00111110
+ > Register A =   0 = 000 = 00000000
+ > Register A =   0 = 000 = 00000000
 ++ 1: 
  > Register H:L =   3: 18, Data:   0 = 000 = 00000000
----
  > Register A =  18 = 022 = 00010010
  > Register A =   3 = 003 = 00000011
                                     ;
