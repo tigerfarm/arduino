9be7a26727421da9366b092c09be6c764255d063
tigerfarm <tigerfarm@gmail.com>
2021-02-05 23:02:07 -0800

    updates

---
+++ Altair101/asm/test/KtbBasic.h
@@ -0,0 +1,16 @@
+// -----------------------------------------------------------------------------
+// Altair 8800 Simulator
+// Copyright (C) 2017 David Hansel
+// -----------------------------------------------------------------------------
+
+#ifndef PROG_GAMES_H
+#define PROG_GAMES_H
+
+#include <Arduino.h>
+
+uint16_t prog_games_copy_killbits();
+uint16_t prog_games_copy_pong();
+uint16_t prog_games_copy_pongterm();
+uint16_t prog_games_copy_daisy();
+
+#endif
