df7aead3f8e9130999b70574ec554d0e61c797ec
tigerfarm <tigerfarm@gmail.com>
2021-03-20 00:31:23 -0700

    updates

--- Altair101/asm/programs/pCliPlayer.asm
+++ Altair101/asm/programs/pCliPlayer.asm
@@ -1,9 +1,8 @@
-                                        ; --------------------------------------
+                                        ; ----------------------------------------
                                         ; Command line interface program
                                         ;   to make player OUT opcode calls.
                                         ;
-                                        ; --------------------------------------
-                                        ; --------------------------------------
+; --------------------------------------------------------------------------------
                 lxi sp,STACK            ; Set stack pointer
                 lxi h,lb                ; Set the H:L register pair to the line buffer.
                 shld lbc                ; Store H and L registers into memory, the line buffer counter.
@@ -11,6 +10,7 @@
                 lxi h,StartMsg
                 call printStr
                 call HelpMenu
+; --------------------------------------------------------------------------------
     startNewline:
                 mvi a,'\r'
                 out PRINT_PORT
@@ -19,16 +19,12 @@
     startPrompt:
                 lxi h,thePrompt         ; Print the prompt.
                 call printStr
-                                        ;
-                ;lda cpc
-                ;jz mtLine               ; If nothing entered in the line (MT line), nothing to process.
-                ;call processLine
     mtLine:
                 mvi a,0                 ; Initialize cursor line position counter.
                 sta cpc
                 lxi h,0                 ; Set line buffer address counter to zero.
                 shld lbc
-                                        ; --------------------------------------
+; --------------------------------------------------------------------------------
                                         ; Get an input byte
                                         ; + Ctrl+c to exit.
                                         ; + Process other control keys
@@ -55,16 +51,16 @@
                 jz Menu6
                 cpi '7'
                 jz Menu7
-                                        ; ---------------
+                                        ; ----------------------------------------
                                         ; Handle special keys.
                 cpi 3                   ; Ctrl+c will exit this loop.
                 jz ExitGetByte
                 cpi 13                  ; If carriage return, send line feed and print the prompt.
-                jz startNewline
+                jz processLine
                 cpi 127                 ; Handle Backspace.
                 jz backSpace
-                cpi 7                   ; Ctrl+g, bell.
-                jz bell
+                cpi 12                  ; Ctrl+l, Clear screen.
+                jz clear
                 cpi 7                   ; Ctrl+g, bell.
                 jz bell
                                         ; ---------------
@@ -78,7 +74,71 @@
                 out PRINT_PORT          ; output the character and get a new one.
                 call inrCpc             ; Increment the cursor position.
                 jmp GetByte
-                                        ; -----------------------------------------------------------------
+                                        ; ----------------------------------------
+    bell:
+                out PRINT_PORT          ; Bell tone.
+                jmp GetByte
+                                        ; ---------------
+    backSpace:
+                lda cpc
+                cpi 0                   ; Don't backspace over the prompt.
+                jz GetByte
+                                        ;
+                mvi a,esc               ; Esc[ValueD : Move the cursor left n lines (left one space).
+                out PRINT_PORT
+                mvi a,'['
+                out PRINT_PORT
+                mvi a,'1'
+                out PRINT_PORT
+                mvi a,'D'
+                out PRINT_PORT
+                                        ;
+                mvi a,' '               ; Overwrite the character with a blank.
+                out PRINT_PORT
+                                        ;
+                mvi a,esc               ; Move back to the blank space.
+                out PRINT_PORT
+                mvi a,'['
+                out PRINT_PORT
+                mvi a,'1'
+                out PRINT_PORT
+                mvi a,'D'
+                out PRINT_PORT
+                                        ;
+                call dcrCpc
+                jmp GetByte
+                                        ; ----------------------------------------
+                                        ; Move the cursor home "Esc[H" and clear the screen "Esc[2J".
+    clear:
+                mvi a,esc
+                out PRINT_PORT
+                mvi a,'['
+                out PRINT_PORT
+                mvi a,'H'
+                out PRINT_PORT
+                mvi a,esc
+                out PRINT_PORT
+                mvi a,'['
+                out PRINT_PORT
+                mvi a,'2'
+                out PRINT_PORT
+                mvi a,'J'
+                out PRINT_PORT
+                jmp startNewline
+                                        ;
+; --------------------------------------------------------------------------------
+                                        ; Process a command line entry.
+    processLine:
+                lda cpc
+                cpi 0
+                jz startNewline         ; If nothing entered in the line (MT line), nothing to process.
+                                        ;
+                call printNewline
+                lxi h,processMsg
+                call printStr
+                jmp startNewline
+                                        ;
+; --------------------------------------------------------------------------------
     Menu1:
                 lxi h,Menu1str
                 call printStr
@@ -123,140 +183,6 @@
                 lxi h,PlayCompleted
                 call printStr
                 jmp startPrompt
-
-                                        ; -----------------------------------------------------------------
-    bell:
-                out PRINT_PORT          ; Bell tone.
-                jmp GetByte
-                                        ; ---------------
-    backSpace:
-                lda cpc
-                cpi 0                   ; Don't backspace over the prompt.
-                jz GetByte
-                                        ;
-                mvi a,esc               ; Esc[ValueD : Move the cursor left n lines (left one space).
-                out PRINT_PORT
-                mvi a,'['
-                out PRINT_PORT
-                mvi a,'1'
-                out PRINT_PORT
-                mvi a,'D'
-                out PRINT_PORT
-                                        ;
-                mvi a,' '               ; Overwrite the character with a blank.
-                out PRINT_PORT
-                                        ;
-                mvi a,esc               ; Move back to the blank space.
-                out PRINT_PORT
-                mvi a,'['
-                out PRINT_PORT
-                mvi a,'1'
-                out PRINT_PORT
-                mvi a,'D'
-                out PRINT_PORT
-                                        ;
-                call dcrCpc
-                jmp GetByte
-                                        ; --------------------------------------
-    clear:
-                call clr
-                jmp startNewline
-                                        ; --------------------------------------
-                                        ; Store the key press values to the line buffer and increment the counter.
-        inrCpc:
-                lhld lbc                ; Load H and L registers from memory.
-                mov m,a                 ; Move register A to the H:L address. A -> (HL).
-                inx h                   ; Increment H:L register pair.
-                shld lbc                ; Store H and L registers into memory.
-                                        ;
-                lda cpc                 ; Increment the cursor position counter
-                inr a
-                sta cpc
-                ret
-                                        ; --------------------------------------
-                                        ; Decrement the position counter and store the key press values to the line buffer.
-        dcrCpc:
-                lda cpc
-                dcr a
-                sta cpc
-                ret
-                                        ;
-                                        ; --------------------------------------
-    ExitGetByte:                        ; Exit the input loop.
-                lxi h,ExitMsg
-                call printStr
-                                        ; --------------------------------------
-                hlt 
-                jmp Start 
-                                        ; --------------------------------------
-                                        ; --------------------------------------
-                                        ; Subroutines
-                                        ; 
-                                        ; --------------------------------------
-    processLine:
-                lxi h,echoMsg
-                call printStr
-                call printNewline
-                ret
-                                        ; --------------------------------------
-                                        ; Print a string.
-    printStr:
-                mov a,m                 ; Move the data from H:L address to register A. (HL) -> A. 
-                cpi TERMB               ; Compare to see if it's the string terminate byte.
-                jz sPrintDone
-                out PRINT_PORT          ; Out register A to the serial port.
-                inx h                   ; Increment H:L register pair.
-                jmp printStr
-    sPrintDone:
-                ret
-                                        ; --------------------------------------
-    printNewline:
-                mvi a,'\r'
-                out PRINT_PORT
-                mvi a,'\n'
-                out PRINT_PORT
-                ret
-                                        ; --------------------------------------
-                                        ; Move the cursor home "Esc[H" and clear the screen "Esc[2J".
-    clr:
-                mvi a,esc
-                out PRINT_PORT
-                mvi a,'['
-                out PRINT_PORT
-                mvi a,'H'
-                out PRINT_PORT
-                mvi a,esc
-                out PRINT_PORT
-                mvi a,'['
-                out PRINT_PORT
-                mvi a,'2'
-                out PRINT_PORT
-                mvi a,'J'
-                out PRINT_PORT
-                ret
-                                        ; --------------------------------------
-                                        ;
-    StartMsg    db      '\r\n+++ Program to test MP3 player opcode options.\r\n\r\n'
-                db      0
-    thePrompt   db      '> '
-                db      0
-    echoMsg     db      '++ Echo not implemented, yet(v1).'
-                db      0
-    ExitMsg     db      '\r\n+ Later.\r\n'
-                db      0
-                                        ;
-    PlayCompleted db    '\r\n+ MP3 finished playing.\r\n'
-                db      000h
-                                        ;
-    TERMB       equ     0               ; String terminator.
-    esc         equ     27              ; Escape character, which is 0x1B (hex).
-    ;
-    cpc         ds      1               ; Cursor position counter variable.
-    lbc         ds      2               ; Address of last added key value = lb address + cpc-1.
-    lb          ds      80              ; Place to store what is typed in, for the current line.
-                                        ; Cursor position is also the length of the entered text.
-                                        ;
-                                        ; ----------------------------------------
                                         ; ----------------------------------------
                                         ; Menu
     HelpMenu:
@@ -294,7 +220,7 @@
     Menu3str    db      '  3. Continue playing the MP3.     (start)\r\n'
                 db      0
     MenuBar     db      '  ------\r\n'
-                db      000h
+                db      0
     Menu4str    db      '  4. Loop play MP3 file number.    (loop)\r\n'
                 db      0
     Menu5str    db      '  5. Pause MP3.                    (pause)\r\n'
@@ -304,9 +230,81 @@
     Menu7str    db      '  7. Play MP3 until it is completed before moving to the next opcode.\r\n'
                 db      0
     MenuEnd     db      '\r\n  ------'
-                db      '\r\n  Ctrl+c to exit.\r\n'
-                db      000h            ; Indicator of the end of a text block.
+                db      '\r\n  Ctrl+l : clear screen.'
+                db      '\r\n  Ctrl+c : exit.\r\n'
+                db      0
                                         ;
+; --------------------------------------------------------------------------------
+                                        ; Store the key press values to the line buffer and increment the counter.
+    inrCpc:
+                lhld lbc                ; Load H and L registers from memory.
+                mov m,a                 ; Move register A to the H:L address. A -> (HL).
+                inx h                   ; Increment H:L register pair.
+                shld lbc                ; Store H and L registers into memory.
+                                        ;
+                lda cpc                 ; Increment the cursor position counter
+                inr a
+                sta cpc
+                ret
+                                        ; --------------------------------------
+                                        ; Decrement the position counter and store the key press values to the line buffer.
+    dcrCpc:
+                lda cpc
+                dcr a
+                sta cpc
+                ret
+                                        ; --------------------------------------
+    ExitGetByte:                        ; Exit the input loop.
+                lxi h,ExitMsg
+                call printStr
+                hlt 
+                jmp Start 
+; --------------------------------------------------------------------------------
+                                        ; Subroutines
+                                        ; --------------------------------------
+                                        ; Print a string.
+    printStr:
+                mov a,m                 ; Move the data from H:L address to register A. (HL) -> A. 
+                cpi TERMB               ; Compare to see if it's the string terminate byte.
+                jz sPrintDone
+                out PRINT_PORT          ; Out register A to the serial port.
+                inx h                   ; Increment H:L register pair.
+                jmp printStr
+    sPrintDone:
+                ret
+                                        ; --------------------------------------
+    printNewline:
+                mvi a,'\r'
+                out PRINT_PORT
+                mvi a,'\n'
+                out PRINT_PORT
+                ret
+                                        ; --------------------------------------
+                                        ; --------------------------------------
+                                        ;
+    StartMsg    db      '\r\n+++ Program to test MP3 player opcode options.\r\n\r\n'
+                db      0
+    thePrompt   db      '?- '
+                db      0
+    processMsg  db      '++ processLine, Not implemented, yet.'
+                db      0
+    ExitMsg     db      '\r\n+ Later.\r\n'
+                db      0
+                                        ;
+    PlayCompleted db    '+ MP3 finished playing.\r\n'
+                db      0
+                                        ;
+    TERMB       equ     0               ; String terminator.
+    esc         equ     27              ; Escape character, which is 0x1B (hex).
+    ;
+    cpc         ds      1               ; Cursor position counter variable.
+    lbc         ds      2               ; Address of last added key value = lb address + cpc-1.
+    ;
+    lineLength  equ     80
+    lb          ds      80              ; Place to store what is typed in, for the current line.
+                                        ; Cursor position is also the length of the entered text.
+                                        ;
+                                        ; ----------------------------------------
                                         ; ----------------------------------------
                                         ; When using port 2,
                                         ;   if port 3 is disable, then it defaults to 3, the default serial port.
